93f6e2fb54e80ebc76c9760aa40f65c3fa88ce43
gpdavidyang <98939304+gpdavidyang@users.noreply.github.com>
2025-10-05 18:41:28 +0900

    DY1005 시스템관리자 화면 개선03

--- app/dashboard/admin/documents/photo-grid/[id]/page.tsx
+++ app/dashboard/admin/documents/photo-grid/[id]/page.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import type { Metadata } from 'next'
 import { requireAdminProfile } from '@/app/dashboard/admin/utils'
 import DownloadLinkButton from '@/components/admin/DownloadLinkButton'
